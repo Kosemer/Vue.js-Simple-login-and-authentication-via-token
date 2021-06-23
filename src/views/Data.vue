d21c98c8227241c4b4880c1059c66911fdc32924
Kosemer <82768146+Kosemer@users.noreply.github.com>
2021-06-23 09:31:56 +0200

    Update Data.vue

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-      <!-- A V-for-al végig tudunk lépkedni a tömbünk elemein, de kell neki egy azonosító is (v-bind:key)-->
+      
     <table class="dataTable">
       <thead>
         <tr>
@@ -24,33 +24,31 @@ export default {
   name: "Login",
 
   components: {
-    //Objektum, ebbe fogjuk megadni azokat a komponenseket, amiket az About használ
+    
   },
 
   data() {
-    //Függvény, ami egy objektumot ad vissza. A komponens betöltésekor értékeli ki. STATIKUS adatok vannak benne.
+    
     return {
-      //title: "home",            //A template-ban tudunk rá hivatkozni így pl.: <p>{{title}}</p>
+      //title: "home",            
       
       tab: [],
     };
   },
 
   computed: {
-    //Objektum, ezen belül tudunk függvényeket meghatározni. Amelyek figyelni fognak a változásokra is.
-    //Ha DINAMIKUS adatra van szükség akkor azt itt kell használni.
+    
   },
 
   created() {
-    //Életciklus hook. A komponens létrehozásakor fut le. Itt szokás beállítani különböző értékeket, amikre szükségünk van.
-    //Lefut minden egyes alkalommal, amikor a komponens létre jön.
+    
     this.getDataAction();
   },
 
   methods: {
-    // Objektum.  Különböző függvényeket tudunk benne definiálni, amiket a komponensünk fog tudni használni.
+    
         getDataAction() {
-          console.log("FUTOOOK");
+          
       axios
         .get("https://otthoni-feladat-backend.herokuapp.com/data" , {
           headers: {
